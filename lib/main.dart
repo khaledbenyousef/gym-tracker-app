eb8e7ae7daf2aa65089981fddb71e22f89d5f9d8
khaledbenyousef <kbenyoussef26@gmail.com>
2026-04-12 12:00:28 +0100

    Add dashboard screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'screens/dashboard_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,17 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Gym Tracker',
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const Scaffold(
-        body: Center(
-          child: Text(
-            'Gym Tracker App',
-            style: TextStyle(fontSize: 24),
-          ),
-        ),
-      ),
+      home: DashboardScreen(),
     );
   }
 }
